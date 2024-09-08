e2143c4612a9c412cd4c5804f1e0cf2cfca4540b
Leonardo Moura <me@leomoura.org>
2024-09-08 18:21:06 -0300

    Check if users has proper score to create bond; create function stubs

--- packages/contracts/src/Contract.sol
+++ packages/contracts/src/Contract.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.13;
 
+import "eas-proxy/IGitcoinPassportDecoder.sol";
+
 interface ITrustBond {
     event FeeUpdated(uint256 newFee);
     event CommunityPoolDeposited(uint256 amount);
@@ -8,6 +10,7 @@ interface ITrustBond {
     event RewardsWithdrawn(address indexed user, uint256 amount);
     event BondCreated(address indexed partner1, address indexed partner2, uint256 amount);
     event BondBroken(address indexed breaker, address indexed partner, uint256 amount);
+    event Paused(bool paused);
 
     // security measure
     function pause() external;
@@ -34,6 +37,100 @@ interface ITrustBond {
     function score(address user) external view returns (uint256);
 }
 
-contract TrustBond {
+contract TrustBond is ITrustBond {
+    uint256 public constant REQUIRED_SCORE = 20;
 
- }
+    address public _owner;
+    bool public _paused;
+    uint256 public _fee;
+    IGitcoinPassportDecoder public _passportDecoder;
+
+    constructor(address owner, IGitcoinPassportDecoder passportDecoder) {
+        _owner = owner;
+        _passportDecoder = passportDecoder;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == _owner, "Only owner can call this function");
+        _;
+    }
+
+    modifier onlyWhenNotPaused() {
+        require(!_paused, "Contract is paused");
+        _;
+    }
+
+    function setFee(uint256 newFee) external onlyOwner {
+        _fee = newFee;
+        emit FeeUpdated(_fee);
+    }
+
+    function pause() external onlyOwner {
+        _paused = true;
+        emit Paused(_paused);
+    }
+
+    function unpause() external onlyOwner {
+        _paused = false;
+        emit Paused(_paused);
+    }
+
+    function isPaused() external view returns (bool) {
+        return _paused;
+    }
+
+    function depositCommunityPool(uint256 amount) external onlyOwner {
+        
+    }
+    
+    function withdrawCommunityPool(uint256 amount) external onlyOwner {
+
+    }
+
+    function withdrawRewardsDistribution(address user) external onlyOwner {
+
+    }
+
+    function deposit(uint256 amount, address partner) external onlyWhenNotPaused {
+        require(amount > 0, "Amount must be greater than 0");
+        require(msg.sender != partner, "Cannot bond with yourself");
+        require(_passportDecoder.getScore(msg.sender) >= REQUIRED_SCORE, "Score must be greater");
+        require(_passportDecoder.getScore(partner) >= REQUIRED_SCORE, "Score must be greater");
+    }
+    
+    function withdraw(address partner) external onlyWhenNotPaused {
+
+    }
+
+    function breakBond(address partner) external onlyWhenNotPaused {
+
+    }
+
+    function approve(address partner, address spender) external onlyWhenNotPaused {
+
+    }
+
+    function fee() external view returns (uint256) {
+        return _fee;
+    }
+
+    function communityPoolBalance() external view returns (uint256) {
+        
+    }
+
+    function bonds(address user) external view returns (address[] memory) {
+
+    }
+
+    function personMultiplier(address user) external view returns (uint256) {
+
+    }
+
+    function score(address user) external view returns (uint256) {
+
+    }
+
+    function bond(address partner1, address partner2) external view returns (uint256 amount, uint256 createdAt, uint256 lastUpdated) {
+
+    }
+}
--- packages/contracts/test/Contract.t.sol
+++ packages/contracts/test/Contract.t.sol
@@ -3,21 +3,44 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 
-import "src/Contract.sol";
+import "../src/Contract.sol";
+import "./GitcoinPassportDecoderMock.sol";
 
 contract TestContract is Test {
     TrustBond c;
+    GitcoinPassportDecoderMock passportDecoder;
+    uint256 private userCounter;
 
     function setUp() public {
-        c = new TrustBond();
+        passportDecoder = new GitcoinPassportDecoderMock();
+        c = new TrustBond(address(passportDecoder), passportDecoder);
+        userCounter = 0;
     }
 
-    function testBar() public {
-        assertEq(uint256(1), uint256(1), "ok");
+    function testCantCreateBondWithoutPassport() public {
+        vm.expectRevert();
+        c.deposit(100, getUser());
     }
 
-    function testFoo(uint256 x) public {
-        vm.assume(x < type(uint128).max);
-        assertEq(x + x, x * 2);
+    function testCantCreateBondWithUserWithoutPassport() public {
+        address user = getUser();
+        passportDecoder.setScore(user, 100);
+        vm.expectRevert();
+        vm.prank(user);
+        c.deposit(100, getUser());
+    }
+
+    function testCanCreateBondWithUserWithPassport() public {
+        address user = getUser();
+        passportDecoder.setScore(user, 30);
+        address user2 = getUser();
+        passportDecoder.setScore(user2, 30);
+        vm.prank(user);
+        c.deposit(100, user2);
+    }
+
+    function getUser() internal returns (address) {
+        userCounter++;
+        return address(uint160(uint256(keccak256(abi.encodePacked(block.timestamp, block.difficulty, userCounter)))));
     }
 }
