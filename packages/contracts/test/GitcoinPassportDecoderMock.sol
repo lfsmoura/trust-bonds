60a07b1ce6ae18a4a1155629145c73d5ff03ebd5
Leonardo Moura <me@leomoura.org>
2024-09-14 18:25:49 -0300

    add mock file and remappings

---
+++ packages/contracts/test/GitcoinPassportDecoderMock.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.13;
+
+import "eas-proxy/IGitcoinPassportDecoder.sol";
+
+contract GitcoinPassportDecoderMock is IGitcoinPassportDecoder {
+    mapping(address => uint256) private scores;
+
+    function setScore(address user, uint256 score) external {
+        scores[user] = score;
+    }
+
+    function getPassport(address user) external pure returns (Credential[] memory) {
+        Credential[] memory credentials = new Credential[](0);
+        return credentials;
+    }
+
+    function getScore(address user) external view returns (uint256) {
+        return scores[user];
+    }
+
+    function isHuman(address user) external view returns (bool) {
+        return scores[user] > 0;
+    }
+}
