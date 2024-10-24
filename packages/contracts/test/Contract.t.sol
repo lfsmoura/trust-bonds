dcb9c124079a1f36fd91d90b01df11a145979733
Maximilian Bretschneider <bretschneider.m@gmail.com>
2024-10-24 16:28:46 -0300

    Sketched test functions for breaking bonds

--- packages/contracts/test/Contract.t.sol
+++ packages/contracts/test/Contract.t.sol
@@ -14,11 +14,17 @@ contract TestContract is Test {
     uint256 private userCounter;
     ERC20 token;
     AavePoolMock pool;
+
     function setUp() public {
         passportDecoder = new GitcoinPassportDecoderMock();
         token = new ERC20Mock("Test", "TEST");
         pool = new AavePoolMock(address(token));
-        c = new TrustBond(address(passportDecoder), passportDecoder, IPool(address(pool)), IERC20(address(token)));
+        c = new TrustBond(
+            address(passportDecoder),
+            passportDecoder,
+            IPool(address(pool)),
+            IERC20(address(token))
+        );
         userCounter = 0;
     }
 
@@ -68,25 +74,82 @@ contract TestContract is Test {
     }
 
     function testFindABond() public {
+        // Generate two user addresses
         address user1 = getUser();
         address user2 = getUser();
+        // Create a bond between the two users
         createValidBond(user1, 100, user2, 100);
+        // Retrieve the bond between the two users
         Bond memory bond = c.bond(user1, user2);
+        // Assert existance of bond
         assertEq(bond.partner, user2);
         Bond memory bondReverse = c.bond(user2, user1);
         assertEq(bondReverse.partner, user1);
     }
 
+    function testBreakABond() public {
+        // Generate two user addresses
+        address user1 = getUser();
+        address user2 = getUser();
+        // Create a bond between the two users
+        createValidBond(user1, 100, user2, 100);
+        // Retrieve the bond between the two users
+        Bond memory bond = c.bond(user1, user2);
+        assertEq(bond.partner, user2);
+        // Simulate the first user making the call
+        vm.prank(user1);
+        // Break the bond between the two users
+        c.breakBond(user2);
+        // Expect the next call to revert because the bond has already been broken
+        vm.expectRevert();
+        // Attempt to break the bond again
+        c.breakBond(user2);
+    }
+
+    function testBreakABondWithoutAPartner() public {
+        // Generate two user addresses
+        address user1 = getUser();
+        address user2 = getUser();
+        // Create a bond between the two users
+        createValidBond(user1, 100, user2, 100);
+        // Retrieve the bond between the two users
+        Bond memory bond = c.bond(user1, user2);
+        assertEq(bond.partner, user2);
+        // Expect the next call to revert for trying to break a bond that has no partner
+        vm.expectRevert();
+        // Simulate the first user making the call
+        vm.prank(user1);
+        // Break a bond without pointing a partner
+        c.breakBond(address(0));
+    }
 
     // ------------------------------------------------------------------------
     // helper functions
     // ------------------------------------------------------------------------
     function getUser() internal returns (address) {
         userCounter++;
-        return address(uint160(uint256(keccak256(abi.encodePacked(block.timestamp, block.prevrandao, userCounter)))));
+        return
+            address(
+                uint160(
+                    uint256(
+                        keccak256(
+                            abi.encodePacked(
+                                block.timestamp,
+                                block.prevrandao,
+                                userCounter
+                            )
+                        )
+                    )
+                )
+            );
     }
 
-    function createValidBond(address user1, uint256 amount1, address user2, uint256 amount2) internal {
+    function createValidBond(
+        address user1,
+        uint256 amount1,
+        address user2,
+        uint256 amount2
+    ) internal {
         // Set the user's passport score to 30
         passportDecoder.setScore(user1, 30);
         passportDecoder.setScore(user2, 30);
