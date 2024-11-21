43eb12e7414c444c5a4158959a3a001008574262
Maximilian Bretschneider <bretschneider.m@gmail.com>
2024-11-21 17:09:04 -0300

    Function testBreakABond() => added checks to ensure that the funds register correctly after the bond is broken. Function testBreakABondTwice() => added checks to ensure that the funds are unaltered after the second break attempt (that is to be reverted as invalid) transaction. Function testBreakABondWithoutAPartner() => added checks to ensure that funds are unaltered after the bond break attempt is unsuccessful due to the lack of a partner. Function testBreakABondWIthWrongPartner() => added checks to ensure that funds are unaltered after the bond break attempt is unsuccessful for being called with a wrong partner. The function call is expected to revert and not affect any of the involved parties nor the community funds.

--- packages/contracts/test/Contract.t.sol
+++ packages/contracts/test/Contract.t.sol
@@ -130,14 +130,38 @@ contract TestContract is Test {
         // Retrieve the bond between the two users
         Bond memory bond = c.bond(user1, user2);
         assertEq(bond.partner, user2);
+        // Retrieve the balance of the community pool before breaking the bond
+        uint256 communityBalanceBeforeBreaking = c.communityPoolBalance();
+        // Retrieve user Balance Before Breaking the Bond
+        uint256 user1BalanceBeforeBreaking = token.balanceOf(user1);
+        // Retrieve the fee for breaking a bond
+        uint256 bondBreakFee = c.fee();
         // Simulate the first user making the call
         vm.prank(user1);
         // Break the bond between the two users
         c.breakBond(user2);
+        // Retrieve user1's balance after breaking the bond
+        uint256 user1BalanceAfterBreaking = token.balanceOf(user1);
+        // Check for user1's balance after breaking the bond
+        assertEq(
+            user1BalanceAfterBreaking,
+            user1BalanceBeforeBreaking + 200 - (bondBreakFee * 100)
+        );
+        // Retrieve the balance of the community pool after breaking the bond
+        uint256 communityBalanceAfterBreaking = c.communityPoolBalance();
+        // Check the balance of the community pool after breaking the bond
+        assertEq(
+            communityBalanceAfterBreaking,
+            communityBalanceBeforeBreaking + (bondBreakFee * 100)
+        );
         // Expect the next call to revert because the bond has already been broken
         vm.expectRevert();
         // Attempt to break the bond again
         c.breakBond(user2);
+        // Check for user1's balance to be unaltered
+        assertEq(token.balanceOf(user1), user1BalanceAfterBreaking);
+        // Check for the community pool balance to be unaltered
+        assertEq(c.communityPoolBalance(), communityBalanceAfterBreaking);
     }
 
     function testBreakABondWithoutAPartner() public {
@@ -149,12 +173,60 @@ contract TestContract is Test {
         // Retrieve the bond between the two users
         Bond memory bond = c.bond(user1, user2);
         assertEq(bond.partner, user2);
+        // Retrieve the balance of the community pool before breaking the bond
+        uint256 communityBalanceBeforeBreaking = c.communityPoolBalance();
+        // Retrieve user1 Balance before attempting to break the bond
+        uint256 user1BalanceBeforeBreaking = token.balanceOf(user1);
+        // Retrieve user2 Balance before attempting to break the bond
+        uint256 user2BalanceBeforeBreaking = token.balanceOf(user2);
         // Expect the next call to revert for trying to break a bond that has no partner
         vm.expectRevert();
         // Simulate the first user making the call
         vm.prank(user1);
         // Break a bond without pointing a partner
         c.breakBond(address(0));
+        // Verify that user1's balance is unaltered
+        assertEq(token.balanceOf(user1), user1BalanceBeforeBreaking);
+        // Verify that user2's balance is unaltered
+        assertEq(token.balanceOf(user2), user2BalanceBeforeBreaking);
+        // Verify the community pool balance to be unaltered
+        assertEq(c.communityPoolBalance(), communityBalanceBeforeBreaking);
+    }
+
+    function testBreakABondWithWrongPartner() public {
+        // Generate three user addresses
+        address user1 = getUser();
+        address user2 = getUser();
+        address user3 = getUser();
+        // Transfer 50 tokens to user3 as to infer existing balance on user3's account
+        token.transfer(user3, 100);
+        // Create a bond between user1 and user2
+        createValidBond(user1, 100, user2, 100);
+        // Retrieve the bond between user1 and user2
+        Bond memory bond = c.bond(user1, user2);
+        assertEq(bond.partner, user2);
+        // Retrieve the balance of the community pool before breaking the bond
+        uint256 communityBalanceBeforeBreaking = c.communityPoolBalance();
+        // Retrieve user1 Balance before attempting to break the bond
+        uint256 user1BalanceBeforeBreaking = token.balanceOf(user1);
+        // Retrieve user2 Balance before attempting to break the bond
+        uint256 user2BalanceBeforeBreaking = token.balanceOf(user2);
+        // Retrieve user3 Balance before attempting to break the bond
+        uint256 user3BalanceBeforeBreaking = token.balanceOf(user3);
+        // Expect the next call to revert for trying to break a bond with the wrong partner
+        vm.expectRevert();
+        // Simulate the first user making the call
+        vm.prank(user1);
+        // Attempt to break a bond with the wrong partner
+        c.breakBond(user3);
+        // Check for user1's balance to be unaltered
+        assertEq(token.balanceOf(user1), user1BalanceBeforeBreaking);
+        // Check for user2's balance to be unaltered
+        assertEq(token.balanceOf(user2), user2BalanceBeforeBreaking);
+        // Check for the community pool balance to be unaltered
+        assertEq(c.communityPoolBalance(), communityBalanceBeforeBreaking);
+        // Check for user3's balance to be unaltered
+        assertEq(token.balanceOf(user3), user3BalanceBeforeBreaking);
     }
 
     // ------------------------------------------------------------------------
